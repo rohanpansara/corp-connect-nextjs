c998648f0c00db0b8480a9aac14ac1a8625789c4
Rohan Pansara <rohanpansara2632@gmail.com>
2024-12-02 20:23:54 +0530

    Added cards below left side cards

--- src/app/(with-sidebar)/dashboard/page.tsx
+++ src/app/(with-sidebar)/dashboard/page.tsx
@@ -8,11 +8,12 @@ const DashboardPage = () => {
   return (
     <div className="min-h-screen w-full bg-mainBackground flex items-center flex-col space-y-4 p-4">
       <WelcomeHeaderWrapper />
-      <div className="w-full max-w-full flex">
-        <div className="grid md:grid-cols-1 xl:grid-cols-2 mr-auto w-[50%] max-w-[50%]">
+      <div className="w-full max-w-full flex h-[200px]">
+        <div className="grid md:grid-cols-1 xl:grid-cols-2 mr-auto w-[50%] max-w-[50%] gap-y-[16px]">
+          <SideCardsWrapper />
           <SideCardsWrapper />
         </div>
-        <div className="flex-grow flex h-[200px] bg-mainBackground">
+        <div className="flex-grow flex h-full bg-mainBackground">
           <RightSideCards />
         </div>
       </div>
--- src/components/dashboard/RightCards.tsx
+++ src/components/dashboard/RightCards.tsx
@@ -5,7 +5,7 @@ import { DonutChart } from "../chart/DonutChart";
 const RightSideCards = () => {
   return (
     <>
-      <Card className="w-1/2 h-full mr-2 p-2 bg-[#cfe2e2] dark:bg-[#2CA58D] shadow-lg rounded-md">
+      <Card className="w-1/2 h-[216px] mr-2 p-2 bg-[#cfe2e2] dark:bg-[#2CA58D] shadow-lg rounded-md">
         <CardHeader className="flex flex-row justify-between items-center p-1">
           <span className="text-sm font-medium text-gray-600">Work Hours</span>
           <div className="flex items-center w-[100px] border-[1px] overflow-hidden border-gray-600 dark:border-gray-300 rounded-[4px]">
@@ -20,7 +20,7 @@ const RightSideCards = () => {
           </CardDescription>
         </CardContent>
       </Card>
-      <Card className="w-1/2 h-full ml-2 p-2 bg-[#cfe2e2] dark:bg-[#2CA58D] shadow-lg rounded-md">
+      <Card className="w-1/2 h-[216px] ml-2 p-2 bg-[#cfe2e2] dark:bg-[#2CA58D] shadow-lg rounded-md">
         <CardHeader></CardHeader>
       </Card>
     </>
