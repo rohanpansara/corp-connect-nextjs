92761f33af57e3613cb01e9917893be79afd7c85
Rohan Pansara <rohanpansara2632@gmail.com>
2024-12-04 21:02:17 +0530

    Added static data to the work hours card

--- src/app/(with-sidebar)/dashboard/page.tsx
+++ src/app/(with-sidebar)/dashboard/page.tsx
@@ -9,7 +9,7 @@ const DashboardPage = () => {
     <div className="min-h-screen w-full bg-mainBackground flex items-center flex-col space-y-4 p-4">
       <WelcomeHeaderWrapper />
       <div className="w-full max-w-full flex h-[200px]">
-        <div className="grid md:grid-cols-1 xl:grid-cols-2 mr-auto w-[50%] max-w-[50%] gap-y-[16px]">
+        <div className="grid md:grid-cols-2 xl:grid-cols-2 mr-auto w-[50%] max-w-[50%] gap-y-[16px]">
           <SideCardsWrapper />
           <SideCardsWrapper />
         </div>
--- src/components/dashboard/RightCards.tsx
+++ src/components/dashboard/RightCards.tsx
@@ -1,24 +1,26 @@
-import { Card, CardContent, CardDescription, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader } from "@/components/ui/card";
 import AttendanceToggleButtons from "./AttendanceToggle";
-import { DonutChart } from "../chart/DonutChart";
 
 const RightSideCards = () => {
   return (
     <>
       <Card className="w-1/2 h-[216px] mr-2 p-2 bg-[#cfe2e2] dark:bg-[#2CA58D] shadow-lg rounded-md">
         <CardHeader className="flex flex-row justify-between items-center p-1">
-          <span className="text-sm font-medium text-gray-600">Work Hours</span>
+          <span className="text-sm font-medium text-gray-600 dark:text-gray-800">Work Hours</span>
           <div className="flex items-center w-[100px] border-[1px] overflow-hidden border-gray-600 dark:border-gray-300 rounded-[4px]">
             <AttendanceToggleButtons />
           </div>
         </CardHeader>
-        <CardContent className="h-full w-full flex items-center justify-center">
-          <CardDescription>
-            {/* <DonutChart >
-
-            </DonutChart> */}
+        <CardContent className="h-[70%] pb-0 w-full flex items-center justify-center">
+          <CardDescription className="h-full flex justify-center items-center">
+            <div className="w-full flex justify-center items-center">
+              <span className="font-bold text-7xl text-gray-600 dark:text-gray-800">7.5</span>
+            </div>
           </CardDescription>
         </CardContent>
+        <CardFooter className="p-1 w-full flex justify-center items-center">
+          <span className="text-sm text-gray-600 dark:text-gray-800">On time today</span>
+        </CardFooter>
       </Card>
       <Card className="w-1/2 h-[216px] ml-2 p-2 bg-[#cfe2e2] dark:bg-[#2CA58D] shadow-lg rounded-md">
         <CardHeader></CardHeader>
