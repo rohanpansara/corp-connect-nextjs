b5063f9551d6ef82411d9161dc25ace1cc3525c6
Rohan Pansara <rohanpansara2632@gmail.com>
2025-01-22 20:55:35 +0530

    Admin Settings Quick Links Icon Added

--- src/app/(with-sidebar)/settings/page.tsx
+++ src/app/(with-sidebar)/settings/page.tsx
@@ -6,11 +6,11 @@ export const metadata = attendanceMetadata;
 
 const SettingsPage = () => {
   return (
-    <div className="min-h-screen w-full bg-mainBackground flex flex-col items-center space-y-4 p-4">
+    <div className="h-[calc(100vh-30px)] w-full bg-mainBackground flex flex-col items-center space-y-4 p-4">
       <div className="h-[30px] w-full max-w-full">
         <PageHeader pageName="Settings" />
       </div>
-      <div className="flex-grow w-full flex items-center justify-center">
+      <div className="h-[calc(100vh-30px)] w-full flex items-center justify-center overflow-hidden">
         <SettingsWidgetsGrid />
       </div>
     </div>
--- src/components/settings/WidgetsGrid.tsx
+++ src/components/settings/WidgetsGrid.tsx
@@ -1,24 +1,140 @@
+import { RiBuilding2Fill, RiListSettingsFill } from "@remixicon/react";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { IoVideocam } from "react-icons/io5";
+import { MdOutlineWeb } from "react-icons/md";
+import { PiCalendarDotFill, PiCalendarStarFill, PiDoorFill, PiSubtitlesFill, PiTimerFill } from "react-icons/pi";
+import { HiReceiptRefund } from "react-icons/hi2";
+import { LuCalendarClock } from "react-icons/lu";
+
 const SettingsWidgetsGrid = () => {
-
-    return (
-        <div className="grid grid-cols-4 md:grid-cols-2 lg:grid-cols-3 w-full min-h-screen gap-2">
-            <div className="w-full h-full bg-green-300 rounded-md"></div>
-            <div className="w-full h-full bg-green-300 rounded-md"></div>
-            <div className="w-full h-full bg-green-300 rounded-md"></div>
-            <div className="w-full h-full bg-green-300 rounded-md"></div>
-
-            <div className="w-full h-full bg-red-300 rounded-md"></div>
-            <div className="w-full h-full bg-red-300 rounded-md"></div>
-            <div className="w-full h-full bg-red-300 rounded-md"></div>
-            <div className="w-full h-full bg-red-300 rounded-md"></div>
-
-            <div className="w-full h-full bg-violet-300 rounded-md"></div>
-            <div className="w-full h-full bg-violet-300 rounded-md"></div>
-            <div className="w-full h-full bg-violet-300 rounded-md"></div>
-            <div className="w-full h-full bg-violet-300 rounded-md"></div>
+  return (
+    <div className="grid grid-cols-4 md:grid-cols-2 lg:grid-cols-3 w-full h-full gap-2">
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#d6dde2]">
+            <RiListSettingsFill className="w-6 h-6 fill-[#4682B4]"/>
+          </div>
+          <div></div>
         </div>
-    )
+      </div>
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#e0d6e2]">
+            <MdOutlineWeb className="w-6 h-6 fill-[#8c46b4]"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#d6e2d9]">
+            <RiBuilding2Fill className="w-6 h-6 fill-[#46b488]"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
 
-}
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#e2dad6]">
+            <PiCalendarStarFill className="w-6 h-6 fill-[#b46046]"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#d6d7e2]">
+            <PiSubtitlesFill className="w-6 h-6 fill-[#6451a9]"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#e2e2d6]">
+            <PiCalendarDotFill className="w-6 h-6 fill-[#8f6138]"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#d6e0e2]">
+            <FaMapLocationDot className="w-6 h-6 fill-[#3e9a98]"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#d6dae2]">
+            <IoVideocam className="w-6 h-6 fill-[#0047AB]"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#e2e1d6]">
+            <PiDoorFill className="w-6 h-6 fill-[#cfb002]"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#dce2d6]">
+            <HiReceiptRefund className="w-6 h-6 fill-[#556B2F]"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#e2d6d6]">
+            <PiTimerFill className="w-6 h-6 fill-[#DC143C]"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#d6dde2]">
+            <RiListSettingsFill className="w-6 h-6"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#d6dde2]">
+            <RiListSettingsFill className="w-6 h-6"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#d6dde2]">
+            <RiListSettingsFill className="w-6 h-6"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="w-full h-full bg-gray-200 rounded-md">
+        <div className="w-full h-full flex justify-between items-center p-1 px-4 rounded-[3px]">
+          <div className="rounded-full p-3 bg-[#d6dde2]">
+            <RiListSettingsFill className="w-6 h-6"/>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default SettingsWidgetsGrid;
