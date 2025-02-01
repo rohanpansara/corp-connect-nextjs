47df6bae00699029ded51419855344ef99f94b92
Rohan Pansara <rohanpansara2632@gmail.com>
2025-02-01 22:03:12 +0530

    Restyled the settings grid for dark mode

--- src/components/settings/WidgetCard.tsx
+++ src/components/settings/WidgetCard.tsx
@@ -15,16 +15,23 @@ const WidgetCard: FC<WidgetProps> = ({ title, Icon, iconFill, bgColor }) => {
 
   return (
     <div
-      className="w-full h-full rounded-md shadow-sm overflow-hidden hover:cursor-pointer hover:shadow-md hover:scale-105 transition-transform"
-      style={{ background: isHovered ? bgColor : "#eef0f1" }}
+      className="w-full h-full rounded-md shadow-sm overflow-hidden hover:cursor-pointer hover:shadow-md hover:scale-105 transition-transform bg-[#FFFFFF] dark:bg-[#35383A]"
+      style={{ background: isHovered ? bgColor : undefined }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className="w-full h-full flex justify-start items-center p-1 px-4 rounded-[3px]">
-        <div className={`rounded-full p-2 sm:p-1 md:p-1.5 lg:p-3 xl:p-4`} style={{ background: bgColor }}>
+        <div
+          className={`rounded-full p-2 sm:p-1 md:p-1.5 lg:p-3 xl:p-4`}
+          style={{ background: bgColor }}
+        >
           <Icon style={{ height: 24, width: 24, fill: iconFill }} />
         </div>
-        <div className="pl-6 text-2xl md:text-xs lg:text-lg xl:text-sm text-slate-500">
+        <div
+          className={`pl-6 text-2xl md:text-xs lg:text-lg xl:text-sm ${
+            isHovered ? "text-slate-600 font-medium" : "text-slate-500 dark:text-slate-400"
+          }`}
+        >
           {title}
         </div>
       </div>
