96570fed7544c9adc0e092cda0f43169ad8c0060
Rohan Pansara <rohanpansara2632@gmail.com>
2025-04-28 13:20:32 +0530

    Updated login page design

--- src/app/(without-sidebar)/auth/new-auth/login/LoginPage.tsx
+++ src/app/(without-sidebar)/auth/new-auth/login/LoginPage.tsx
@@ -4,13 +4,10 @@ import illustration from '@/assets/illustration.png'
 import Image from 'next/image'
 import React, { useEffect, useState } from 'react'
 import { Inter } from 'next/font/google'
-
-// If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['latin'] })
 
 const LoginPage: React.FC = () => {
-
-  const [bgColor, setBgColor] = useState<string>('#1781C2') // Default fallback
+  const [bgColor, setBgColor] = useState<string>('#1781C2')
 
   useEffect(() => {
     const color = localStorage.getItem('bgColor')
@@ -26,8 +23,10 @@ const LoginPage: React.FC = () => {
       >
         <div className='flex flex-col sm:flex-row h-[90%] w-full rounded-md backdrop-blur-md shadow-lg p-2 gap-2 sm:max-w-[90%]'>
           {/* This section will be hidden on small devices */}
-          <section className='hidden sm:relative sm:flex w-full sm:w-[60%] h-[200px] sm:h-full rounded-sm border border-opacity-10 border-gray-800 overflow-hidden'
-          style={{ backgroundColor: bgColor }}>
+          <section
+            className='hidden sm:relative sm:flex w-full sm:w-[60%] h-[200px] sm:h-full rounded-sm border border-opacity-10 border-gray-800 overflow-hidden'
+            style={{ backgroundColor: bgColor }}
+          >
             <Image
               src={illustration}
               alt='Illustration'
@@ -41,43 +40,54 @@ const LoginPage: React.FC = () => {
           <section className='w-full sm:w-[40%] h-full rounded-r-sm'>
             <div className='h-full p-2 gap-2 flex flex-col justify-center items-center'>
               <div className='w-full h-[20%] flex flex-col gap-1 justify-center items-center border-b-[1px] border-gray-800'>
-                <h3 className='text-2xl font-semibold text-gray-100'>Welcome to CorpConnect</h3>
-                <span className='text-sm text-gray-300'>Login to your account</span>
+                <h3 className='font-semibold text-gray-100 text-md lg:text-2xl'>
+                  Welcome to{' '}
+                  <span className='py-1 px-2 text-blue-200 bg-blue-50 bg-opacity-20 rounded-md'>
+                    CorpConnect
+                  </span>
+                </h3>
               </div>
               <div className='w-full h-[80%] border-t-[1px] border-gray-800'>
-                <form className='flex flex-col gap-2 p-2 justify-evenly h-full'>
+                <form className='flex flex-col p-2 justify-between h-full'>
                   <div className='flex flex-col gap-1'>
-                    <label htmlFor='email' className='text-sm text-gray-600'>
-                      Email
-                    </label>
-                    <input
-                      type='email'
-                      id='email'
-                      className='border-[1px] border-gray-800 rounded-sm p-2'
-                      required
-                    />
+                    <div className='flex flex-col gap-1'>
+                      <label htmlFor='email' className='text-sm text-gray-200'>
+                        Email
+                      </label>
+                      <input
+                        type='email'
+                        id='email'
+                        className='border-[1px] border-gray-800 rounded-sm p-1'
+                        required
+                      />
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                      <label htmlFor='password' className='text-sm text-gray-200'>
+                        Password
+                      </label>
+                      <input
+                        type='password'
+                        id='password'
+                        className='border-[1px] border-gray-800 rounded-sm p-1'
+                        required
+                      />
+                    </div>
                   </div>
                   <div className='flex flex-col gap-1'>
-                    <label htmlFor='password' className='text-sm text-gray-600'>
-                      Password
-                    </label>
-                    <input
-                      type='password'
-                      id='password'
-                      className='border-[1px] border-gray-800 rounded-sm p-2'
-                      required
-                    />
-                  </div>
-                  <div className='flex flex-col gap-1'>
-                    <label htmlFor='remember' className='text-sm text-gray-600'>
-                      <input type='checkbox' id='remember' className='mr-2' />
-                      Remember me
-                    </label>
-                  </div>
-                  <div className='flex flex-col gap-1'>
-                    <button type='submit' className='bg-blue-600 text-white rounded-sm p-2 hover:bg-blue-700'>
-                      Login
-                    </button>
+                    <div className='flex flex-col gap-1'>
+                      <label htmlFor='remember' className='flex items-center text-sm text-gray-200'>
+                        <input type='checkbox' id='remember' className='mr-2 rounded-md focus:outline-none' />
+                        <span className='text-sm text-gray-200'>Remember me</span>
+                      </label>
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                      <button
+                        type='submit'
+                        className='bg-blue-600 text-white rounded-sm p-2 hover:bg-blue-700'
+                      >
+                        Login
+                      </button>
+                    </div>
                   </div>
                 </form>
               </div>
